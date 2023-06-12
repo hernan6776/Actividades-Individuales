34541b5425fb23fa0cbc37b353a56a38bb10f966
hernan6776 <hernanferrero1@gmail.com>
2023-06-12 11:34:46 -0300

    Esta es la tercera carga al repositorio de github con la resolucion del ejercicio 4 de elegir tu propia aventura.

--- Clase_6/EjerciciosClaseActividades.java
+++ Clase_6/EjerciciosClaseActividades.java
@@ -31,16 +31,35 @@ public class EjerciciosClaseActividades {
 
 /* 4 - Crear un programa que cuente una historia al estilo de “Elige tu propia Aventura */
 
-        System.out.println("Vamos a jugar a Elige tu propia Aventura.");
-        System.out.println("Es una noche estrellada, te encuentras en un bosque misterioso " +
-                "(Elige una de las dos opciones): ");
+        System.out.println("""
+                    Vamos a jugar a "Elige tu propia Aventura".
+                    Es una noche estrellada, te encuentras en un bosque misterioso.
+                    opcion 1: Exploras la oscuridad en busca de la fuente de un susurro escalofriante.
+                    opcion 2: Sigues el sendero iluminado hacia una pequeña cabaña acogedora.
+                    Elige una de las dos opciones, presionando 1 o 2:""");
+        int opcion = teclado.nextInt();
 
-
-
-
-
-
-
+        if (opcion == 1) {
+            System.out.println("El jugador muere");
+        } else {
+            System.out.println("""
+                    Decides seguir el sendero iluminado hacia la pequeña cabaña acogedora.
+                    Al llegar a la cabaña, te das cuenta de que hay dos puertas:
+                    opcion 1: Entras por la puerta de la izquierda.
+                    opcion 2: Optas por la puerta de la derecha.
+                    Elige una de las dos opciones, presionando 1 o 2""");
+                int opcion2 = teclado.nextInt();
+            if (opcion2 == 1) {
+                System.out.println("""
+                    Al abrir la puerta de la izquierda, te encuentras con una habitación llena de tesoros brillantes y mágicos.
+                    Te maravillas con las joyas y los objetos preciosos que llenan la sala.
+                    Te das cuenta de que has encontrado el legendario tesoro perdido del bosque.
+                    Con tu corazón lleno de emoción y riquezas, decides regresar a casa para compartir tu increíble aventura con el mundo.
+                    ¡Felicidades, has tenido un final exitoso en tu búsqueda!""");
+            } else {
+                System.out.println("el jugador muere");
+            }
+        }
 
 /* 5 - Armar un programa que permita cargar 3 números y mostrar cual es el número promedio */
 
@@ -92,5 +111,3 @@ del juego y mostrar el resultado. */
 
     }
 }
-
-//
